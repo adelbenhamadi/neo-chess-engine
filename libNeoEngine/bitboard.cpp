ffcc3c8a5fd2fc5980219cb99542e4268a9181ef
adelbenhamadi <benhamadi.adel@gmail.com>
2021-05-15 19:03:44 +0100

    refact. bitboard

--- libNeoEngine/bitboard.cpp
+++ libNeoEngine/bitboard.cpp
@@ -2,22 +2,85 @@
 #include <algorithm>
 #include <bitset>
 
+#include "tables.h"
 
-
-namespace bitboard {
-
-	uint8_t PopCnt16[1 << 16];
+namespace tables {
+	
 	Bitboard SquareBB[SQUARE_NB];
 	Bitboard LineBB[SQUARE_NB][SQUARE_NB];
+	uint8_t PopCnt16[1 << 16];
+	uint8_t SquareDistance[SQUARE_NB][SQUARE_NB];
+	Bitboard PseudoAttacks[PIECE_TYPE_NB][SQUARE_NB];
+	Bitboard PawnAttacks[2/*sides*/][SQUARE_NB];
+}
+namespace bitboards {
 
+	using namespace tables;
 
 	void setup()
 	{
-		for (unsigned i = 0; i < (1 << 16); ++i)
-			PopCnt16[i] = uint8_t(std::bitset<16>(i).count());
-
-		for (unsigned s = SQ_A1; s <= SQ_H8; ++s)
+		/*fill 16bit population counter:
+		 PopCnt16[x]  = # of significant bits in x (x from 0 to 65535) */
+		if (!population::USE_POPCNT) {
+			for (unsigned i = 0; i < (1 << 16); ++i)
+				PopCnt16[i] = uint8_t(std::bitset<16>(i).count());
+		}
+		/*fill square bitboards table*/
+		for (int s = SQ_A1; s <= SQ_H8; ++s)
 			SquareBB[s] = (1ULL << s);
-	}
+		/*fill square distances table*/
+		for (int s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+			for (int s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+				SquareDistance[s1][s2] = std::max(distance<File>((Square)s1, (Square)s2), distance<Rank>((Square)s1, (Square)s2));
+
+        /*generate magics! And fill rook/bishop BB tables*/
+
+        init_magics(ROOK, RookTable, RookMagics);
+        init_magics(BISHOP, BishopTable, BishopMagics);
+        /*fill PAWN attacks table and pseudo attacks for KING,KNIGHT,QUEEN,ROOK and BISHOP*/
+        for (int s1 = SQ_A1; s1 <= SQ_H8; ++s1)
+        {
+            PawnAttacks[WHITE][s1] = pawn_attacks_bb<WHITE>(square_bb((Square)s1));
+            PawnAttacks[BLACK][s1] = pawn_attacks_bb<BLACK>(square_bb((Square)s1));
+
+            for (int step : {-9, -8, -7, -1, 1, 7, 8, 9})
+                PseudoAttacks[KING][s1] |= safe_destination((Square)s1, step);
+
+            for (int step : {-17, -15, -10, -6, 6, 10, 15, 17})
+                PseudoAttacks[KNIGHT][s1] |= safe_destination((Square)s1, step);
+
+            PseudoAttacks[BISHOP][s1] = attacks_bb(BISHOP, (Square)s1, 0);
+            PseudoAttacks[ROOK][s1] = attacks_bb(ROOK, (Square)s1, 0);
+            PseudoAttacks[QUEEN][s1] = PseudoAttacks[BISHOP][s1] | PseudoAttacks[ROOK][s1];
+
+            for (EPieceType pt : { BISHOP, ROOK })
+                for (int s2 = SQ_A1; s2 <= SQ_H8; ++s2)
+                    if (PseudoAttacks[pt][s1] & SquareBB[s2])
+                        LineBB[s1][s2] = (attacks_bb(pt, (Square)s1, 0) & attacks_bb(pt, (Square)s2, 0)) | SquareBB[s1] | SquareBB[s2];
+        }
+        	
+    }
+
+    /// Bitboards::pretty() returns an ASCII representation of a bitboard suitable
+    /// to be printed to standard output. Useful for debugging.
+
+    const std::string pretty(Bitboard b) {
+
+        std::string s = "+---+---+---+---+---+---+---+---+\n";
+
+        for (int r = RANK_8; r >= RANK_1; --r)
+        {
+            for (int f = FILE_A; f <= FILE_H; ++f)
+                s += b & square::make_square((File)f,(Rank) r) ? "| X " : "|   ";
+
+            s += "| " + std::to_string(1 + r) + "\n+---+---+---+---+---+---+---+---+\n";
+        }
+        s += "  a   b   c   d   e   f   g   h\n";
+
+        return s;
+    }
+
+    
+
 
 }
--- libNeoEngine/bitboard.h
+++ libNeoEngine/bitboard.h
@@ -1,172 +1,146 @@
+/*
+  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2021 The Stockfish developers (see AUTHORS file)
+
+  Stockfish is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Stockfish is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
+#include <string>
 
-#include <vector>
-#include <iostream>
-#include <math.h>
-#include <sstream>
-#include <chrono>
-#include <assert.h>
-
-#include "types.h"
+#include "bb_utils.h"
 #include "tables.h"
+#include "magic.h"
 
 
+using namespace tables;
+using namespace magics;
+
+namespace bitboards {
+
+    void setup();
+    const std::string pretty(Bitboard b);
+    
+}
 
 
+template<Side side>
+constexpr Bitboard pawn_attacks_bb(Bitboard b) {
+    return (side == WHITE ?
+        ((b & ~MASK_FILE_A) << 7) | ((b & ~MASK_FILE_H) << 9)
+        :
+        ((b & ~MASK_FILE_A) >> 9) | ((b & ~MASK_FILE_H) >> 7)
+        );
+}
 
-#ifdef _MSC_VER
-#  include <nmmintrin.h>
-#  define __builtin_popcountll _mm_popcnt_u64
-#endif
+inline Bitboard pawn_attacks_bb(Side side, Square sq) {
+    assert(square::is_ok(sq));
+    return PawnAttacks[side][sq];
+}
 
-namespace bitboard {
-
-
-
-	inline bool moreThanOne(Bitboard i)
-	{
-		return i & (i - 1);
-	}
-	/*
-	PopulationCount:3 static routines for population count on a given bitboard
-	 populationCount(Bitboard i) from Bit Hacks
-	 populationCount64swar(Bitboard i) : Swar algo for 64b
-	 populationCount64sparse(Bitboard i) : for Sparse bitboards
-	*/
-	struct PopulationCount {
-		// Source : https://graphics.stanford.edu/~seander/bithacks.html
-		static inline unsigned int populationCount(Bitboard i)
-		{
-#ifdef NO_POPCNT
-			return popcount64sparse(Bitboard i);
-#else
-			return (unsigned int)__builtin_popcountll(i);
-#endif
-		}
-
-		// SWAR algorithm for 64 bits
-		static inline unsigned int populationCount64swar(Bitboard i)
-		{
-			i = i - ((i >> 1) & 0x5555555555555555);
-			i = (i & 0x3333333333333333) + ((i >> 2) & 0x3333333333333333);
-			return (((i + (i >> 4)) & 0x0f0f0f0f0f0f0f0f) *
-				0x0101010101010101) >> 56;
-		}
-
-		// "Brian Kernighan's way" https://chessprogramming.wikispaces.com/Population+Count
-		// efficient for sparse bitboard
-		static inline unsigned int populationCount64sparse(Bitboard i)
-		{
-			int c = 0;
-			for (; i > 0U; c++) {
-				i &= i - 1; // reset LS1B
-			}
-			return c;
-		}
-	};
+constexpr Bitboard pawn_fork_attacks_bb(Side side,Bitboard b) {
+    return (side == WHITE ?
+        ((b & ~MASK_FILE_A) << 7) & ((b & ~MASK_FILE_H) << 9)
+        :
+        ((b & ~MASK_FILE_A) >> 7) & ((b & ~MASK_FILE_H) >> 9)
+        );
+}
 
 
-	//see https://chessprogramming.wikispaces.com/BitScan
+/// pawn_double_attacks_bb() returns the squares doubly attacked by pawns of the
+/// given color from the squares in the given bitboard.
 
-	inline Square msb(const Bitboard& i)
-	{
-#ifdef _MSC_VER
-		unsigned long idx;
-		_BitScanReverse64(&idx, i);
-		return (Square)idx;
-
-#else
-		Bitboard idx;
-		__asm__("bsrq %1, %0": "=r"(idx) : "rm"(i));
-
-#endif
-		return (Square)idx;
-
-	}
-	// Assembly code by Heinz van Saanen
-
-	inline Square lsb(Bitboard i) {
-#ifdef _MSC_VER
-		unsigned long idx;
-		_BitScanForward64(&idx, i);
-		return (Square)idx;
-
-#else
-		Bitboard idx;
-		__asm__("bsfq %1, %0": "=r"(idx) : "rm"(i));
-		return (Square)idx;
-
-#endif
-	}
-
-	inline Square pop_lsb(Bitboard* b) {
-		const Square s = lsb(*b);
-		*b &= *b - 1;
-		return s;
-	}
-	inline void setBit(Bitboard& i, Square sq) {
-		i |= (1ULL << sq);
-	}
-	inline void unsetBit(Bitboard& i, Square sq) {
-		i &= ~(1ULL << sq);
-	}
-	inline bool isBitSet(const Bitboard i, Square sq) {
-		assert(square::is_ok(sq));
-		return i & (1ULL << (sq));
-	}
-	inline bool isBitSet(const Bitboard i, const unsigned int file, const unsigned int rank)
-	{
-		Square sq = square::make_square((File)file, (Rank)rank);
-		return isBitSet(i, sq);
-	}
-
-	extern Bitboard SquareBB[SQUARE_NB];
-	extern Bitboard LineBB[SQUARE_NB][SQUARE_NB];
-
-	/// line_bb() returns a bitboard representing an entire line (from board edge
-	/// to board edge) that intersects the two given squares. If the given squares
-	/// are not on a same file/rank/diagonal, the function returns 0. For instance,
-	/// line_bb(SQ_C4, SQ_F7) will return a bitboard with the A2-G8 diagonal.
-	inline Bitboard line_bb(Square s1, Square s2) {
-		assert(square::is_ok(s1) && square::is_ok(s2));
-		return LineBB[s1][s2];
-	}
-
-	/// aligned() returns true if the squares s1, s2 and s3 are aligned either on a
-	inline bool aligned(Square s1, Square s2, Square s3) {
-		return line_bb(s1, s2) & SquareBB[s3];
-		//	return LineBB[s1][s2] & SquareBB[s3];
-	}
+template<Side C>
+constexpr Bitboard pawn_double_attacks_bb(Bitboard b) {
+    return C == WHITE ? ((b & ~MASK_FILE_A) << 7)& ((b & ~MASK_FILE_H) << 9)
+        : ((b & ~MASK_FILE_A) >> 9)& ((b & ~MASK_FILE_H) >> 7);
+}
 
 
+/// adjacent_files_bb() returns a bitboard representing all the squares on the
+/// adjacent files of a given square.
+constexpr Bitboard adjacent_files_bb(Square s) {
+    return ((file_bb(s) & ~MASK_FILE_H) << 1) | ((file_bb(s) & ~MASK_FILE_A) >> 1);
+}
 
 
-	void setup();
+/// line_bb() returns a bitboard representing an entire line (from board edge
+/// to board edge) that intersects the two given squares. If the given squares
+/// are not on a same file/rank/diagonal, the function returns 0. For instance,
+/// line_bb(SQ_C4, SQ_F7) will return a bitboard with the A2-G8 diagonal.
+inline Bitboard line_bb(Square s1, Square s2) {
 
-	//Operator overloads for Bitboard and Square
-	inline Bitboard operator&(Bitboard b, Square s) {
-		return b & SquareBB[s];
-	}
+    assert(square::is_ok(s1) && square::is_ok(s2));
+    return tables::LineBB[s1][s2];
+}
 
-	inline Bitboard operator|(Bitboard b, Square s) {
-		return b | SquareBB[s];
-	}
 
-	inline Bitboard operator^(Bitboard b, Square s) {
-		return b ^ SquareBB[s];
-	}
+/// between_bb() returns a bitboard representing squares that are linearly
+/// between the two given squares (excluding the given squares). If the given
+/// squares are not on a same file/rank/diagonal, we return 0. For instance,
+/// between_bb(SQ_C4, SQ_F7) will return a bitboard with squares D5 and E6.
+inline Bitboard between_bb(Square s1, Square s2) {
+    Bitboard b = line_bb(s1, s2) & ((tables::AllSquares << s1) ^ (tables::AllSquares << s2));
+    return b & (b - 1); //exclude lsb
+}
 
-	inline Bitboard& operator&=(Bitboard& b, Square s) {
-		return b &= SquareBB[s];
-	}
 
-	inline Bitboard& operator|=(Bitboard& b, Square s) {
-		return b |= SquareBB[s];
-	}
+/// forward_ranks_bb() returns a bitboard representing the squares on the ranks
+/// in front of the given one, from the point of view of the given color. For instance,
+/// forward_ranks_bb(BLACK, SQ_D3) will return the 16 squares on ranks 1 and 2.
+constexpr Bitboard forward_ranks_bb(Side c, Square s) {
+    return c == WHITE ? ~MASK_RANK[0] << 8 * square::relative_rank(WHITE, s)
+        : ~MASK_RANK[8] >> 8 * square::relative_rank(BLACK, s);
+}
 
-	inline Bitboard& operator^=(Bitboard& b, Square s) {
-		return b ^= SquareBB[s];
-	}
 
+/// forward_file_bb() returns a bitboard representing all the squares along the
+/// line in front of the given one, from the point of view of the given color.
+constexpr Bitboard forward_file_bb(Side c, Square s) {
+    return forward_ranks_bb(c, s) & file_bb(s);
+}
+
+
+/// pawn_attack_span() returns a bitboard representing all the squares that can
+/// be attacked by a pawn of the given color when it moves along its file, starting
+/// from the given square.
+constexpr Bitboard pawn_attack_span(Side c, Square s) {
+    return forward_ranks_bb(c, s) & adjacent_files_bb(s);
+}
+
+
+/// passed_pawn_span() returns a bitboard which can be used to test if a pawn of
+/// the given color and on the given square is a passed pawn.
+constexpr Bitboard passed_pawn_span(Side c, Square s) {
+    return pawn_attack_span(c, s) | forward_file_bb(c, s);
+}
+
+
+/// aligned() returns true if the squares s1, s2 and s3 are aligned either on a
+/// straight or on a diagonal line.
+inline bool aligned(Square s1, Square s2, Square s3) {
+    return line_bb(s1, s2) & s3;
+}
+
+/// shift() moves a bitboard one or two steps as specified by the direction D
+
+constexpr Bitboard shift(Direction D,Bitboard b) {
+    return  D == NORTH ? b << 8 : D == SOUTH ? b >> 8
+        : D == NORTH + NORTH ? b << 16 : D == SOUTH + SOUTH ? b >> 16
+        : D == EAST ? (b & ~MASK_FILE_H) << 1 : D == WEST ? (b & ~MASK_FILE_A) >> 1
+        : D == NORTH_EAST ? (b & ~MASK_FILE_H) << 9 : D == NORTH_WEST ? (b & ~MASK_FILE_A) << 7
+        : D == SOUTH_EAST ? (b & ~MASK_FILE_H) >> 7 : D == SOUTH_WEST ? (b & ~MASK_FILE_A) >> 9
+        : 0;
 }
