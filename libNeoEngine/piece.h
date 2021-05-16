02dee65c33f097d87adba4fb7d29d9e30428fb61
adelbenhamadi <benhamadi.adel@gmail.com>
2021-05-16 04:19:24 +0100

    charToCode, codeToChar moved to piece.cpp

--- libNeoEngine/piece.h
+++ libNeoEngine/piece.h
@@ -9,17 +9,8 @@
 namespace piece {
     extern const  std::map<char, EPiece> piecesCodesMap;
    
-    constexpr EPiece charToCode(char pc) {
-        return piecesCodesMap.at(pc);
-    }
-    constexpr char codeToChar(EPiece c) {
-        for (auto cod : piecesCodesMap)
-            if (cod.second == c) {
-                return cod.first;
-            }
-
-        return '?';
-    }
+    const EPiece charToCode(char pc);
+    const char codeToChar(EPiece c);
     constexpr EPieceType type_of(EPiece p) { return (EPieceType)(p % 6); }
     constexpr EPiece make_piece(EPieceType pt, Side side) {
         return EPiece(pt + 6 * side);
