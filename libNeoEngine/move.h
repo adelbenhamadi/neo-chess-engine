95cd9fa48aa8ab85a19e8087f0648bbe8d2576db
adelbenhamadi <benhamadi.adel@gmail.com>
2021-05-16 04:16:02 +0100

    added set routine to Move class

--- libNeoEngine/move.h
+++ libNeoEngine/move.h
@@ -29,9 +29,10 @@ enum EMoveType : unsigned {
 
 class Move
 {
-	unsigned int _move;
+	unsigned  _move;
 public:
 
+	int score ;
 
 	//Default constructor for null move
 	inline Move() noexcept : _move(0),score(0) {}
@@ -46,34 +47,68 @@ public:
 			| (promoted  << 16)
 			+ mtype ;
 
-	}
+	}	
 	inline unsigned get() const { return _move; }
+	inline unsigned set(unsigned m)  { _move = m; }
 
 	inline Square from_sq() const { return static_cast<Square>(_move & 0b111111); }
 
 	inline Square to_sq() const { return static_cast<Square>((_move & 0b111111000000) >> 6); }
 
-	inline unsigned int piece_type() const { return (_move & 0b1111000000000000) >> 12; }
+	inline unsigned int captured() const { return (_move & 0b1111000000000000) >> 12; }
 
-	inline unsigned int promoted_piece() const {
+	inline unsigned int promoted_type() const {
 		return (_move & 0b11110000000000000000) >> 16;
 	}
 
-	inline EMoveType type() const { return (EMoveType)(_move >> 20); }
+	inline EMoveType type() const { return (EMoveType)(_move >> 20 << 20); }
 
-	inline unsigned int isDoublePawnPush() const { return type() == DOUBLE_PAWN_PUSH; }
-	inline unsigned int isCastling() const { return type() == CASTLING; }
-	inline unsigned int isCapture() const {
-		return type() & CAPTURE;
-	}
-	inline unsigned int isEnPassant() const { return type() == EP_CAPTURE; }
-	inline unsigned int isPromotion() const { return promoted_piece() /*type() & PROMOTION*/; }
+	inline unsigned int isDoublePawnPush() const { return type() & DOUBLE_PAWN_PUSH; }
+	inline unsigned int isCastling() const { return type() & CASTLING; }
+	//inline unsigned int isCapture() const {		return type() ==  CAPTURE;	}
+	inline unsigned int isEnPassant() const { return type() & EP_CAPTURE; }
+	inline bool isPromotion() const { return promoted_type() != EMPTY; }
 
+	
 	//isQuiet returns true for null AND double pawn push move
 	inline bool isQuiet() const { return !(type() & 0b110); }
 	inline bool isNull() const { return _move == 0U; }
+	inline bool isOk() {
+		Square from = from_sq();
+		Square to = to_sq();
+		return  from!=to  ;
+	}
+	std::string toString() {	
+		std::string res = std::string( square::square_str(from_sq())  + square::square_str(to_sq()) );
+		if (isPromotion()) {
+			res += " nbrqk"[(EPieceType)promoted_type()];
+		}
+		return res;
+	}
+	std::string toAlgebric() {
+		if (isNull()) return "0000";
+		EMoveType m = type();
+		const Square from = from_sq();
+		Square to = to_sq();
+		/*set the correct king target square if it is a castling move*/
+		if (m == CASTLING) {
+			to = square::make_square(to > from ? FILE_G : FILE_C, square::rank_of(from));
+		}
 
+		std::string res = square::square_str(from) + square::square_str(to);
 
+		if (isPromotion()) {
+			res += " nbrqk"[(EPieceType)promoted_type()];
+		}
+		return res;
+	}
+
+	void operator=(Move rhs) {
+		_move = rhs.get();
+	}
+	bool operator==(const Move rhs) {
+		return _move == rhs.get();
+	}
 };
 
 
